9d198c2bf8b13dec430621a9cf25d50bfafb48f9
abrarjahin18 <jahin.rupu@gmail.com>
2022-01-25 20:46:00 +0600

    others commit

--- GitExample And practice/Program.cs
+++ GitExample And practice/Program.cs
@@ -7,6 +7,8 @@ namespace GitExample_And_practice
         static void Main(string[] args)
         {
             Student s1 = new Student();
+            if (s1 != null)
+                Console.WriteLine("student exits");
             Console.WriteLine("student add");
             Console.WriteLine("Hello World!");
         }
